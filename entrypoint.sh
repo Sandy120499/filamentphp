f62f5f2640996381c937e42ed9b0b1b7145a4907
Santhosh Raja <167514230+Sandy120499@users.noreply.github.com>
2025-05-17 10:37:52 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run Composer install (if not already done in image build)
+composer install
+
+# Set ownership
+chown -R www-data:www-data /var/www/html
+
+# Run Laravel migrations
+php artisan migrate
+
+# Start Apache in the foreground (important!)
+exec apache2-foreground
